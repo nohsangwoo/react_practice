f810967ce7714db7c74a4b4d630f6c039603a3ab
nohsangwoo <nsgr12@naver.com>
2020-04-28 01:06:47 +0900

    #배열 렌더링

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import Hello from './Hello';
+
 import './App.css';
+import Wrapper from './Wrapper';
+import Counter from './Counter';
+import InputSample from './InputSample';
+import UserList from './UserList';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem',
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // fragment: 비어있는 이름을 가지고있는 태그
+    // div를 사용해서 css가 꼬일때 또는 맘에 안들대 fragment태그를 이용
+    // fragment를 사용하면 실제로 div로 감싸고 있는 태그가 존재하지 않음
+    // babeljs.io를 사용해서 jsx를 javascript언어로 해석해줌
+    // 변수사용을 원한다면 {}안에 변수를 넣어줌
+    <Wrapper>
+      {/* props 전달방법 */}
+      <Hello name="react" color="red" />
+      <Hello color="red" />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+      <Hello name="react" color="red" isSpecial={true} />
+      <Counter />
+      <InputSample />
+      <UserList />
+    </Wrapper>
   );
 }
 
---
+++ src/Hello.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// 비구조할당으로 넘겨받을수있음
+// 또는 그냥 props로 받고 props.color  ,props.name으로 사용가능
+function Hello({ color, name, isSpecial }) {
+  return (
+    // 자바스크립트 값은 {}중괄호로 감싸고 그것을 jsx 문법에서 또 중괄호로 감싼다
+    <div
+      style={{
+        color,
+      }}
+    >
+      {isSpecial ? <b>특별한</b> : <b>안특벼한</b>}
+      {isSpecial && <b>*</b>}
+      안녕하세요{name}
+    </div>
+  );
+}
+
+// 넘겨받은 props의 값이 없거나 설정안됐을경우 기본값 설정
+Hello.defaultProps = {
+  name: '이름없음',
+};
+
+// Hello라는 component를 내보내 주겠다.
+export default Hello;
